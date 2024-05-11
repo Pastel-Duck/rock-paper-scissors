dc5468d5d7b1fd5597eaa5bf0262282f945c1608
Oussama <pastel.lequack@gmail.com>
2024-05-11 16:19:48 +0100

    Add a logic to play the game for one round

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
+    let computerSelection = Math.floor(Math.random() * 3);
+    switch(computerSelection){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
 }
+
 
 function getHumanChoice(){
     let choice = prompt("Type your choice (Rock, Paper or Scissors)").toLocaleLowerCase();
@@ -13,3 +25,24 @@ function getHumanChoice(){
     }
 };
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanSelection, computerSelection){
+    if(humanSelection == "rock" && computerSelection == "scissors" || humanSelection == "paper" && computerSelection == "rock" || humanSelection == "scissors" && computerSelection == "paper"){
+        return `You win! ${humanSelection} beats ${computerSelection}`;
+    }
+    else if(humanSelection == "scissors" && computerSelection == "rock" || humanSelection == "rock" && computerSelection == "paper" || humanSelection == "paper" && computerSelection == "scissors"){
+        return `You lose! ${computerSelection} beats ${humanSelection}`;
+    }
+    else if(computerSelection == humanSelection){
+        return "Draw!";
+    }
+    else{
+        return "Please select a valid input!";
+    }
+}
+
+playRound(humanSelection, computerSelection);
+
+console.log(playRound(humanSelection, computerSelection));
