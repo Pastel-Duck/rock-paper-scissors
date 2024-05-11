88a5b4181dfa1ecedad56785df4e0f955e255539
Oussama <pastel.lequack@gmail.com>
2024-05-11 15:31:14 +0100

    Add a function that takes a human input

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ function getComputerChoice(){
     return Math.floor(Math.random() * 3);
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let choice = prompt("Type your choice (Rock, Paper or Scissors)").toLocaleLowerCase();
+
+    if(choice == "rock" || choice == "paper" || choice == "scissors"){
+        return choice;
+    }
+    else{
+        return alert("Please input a valid choice!");
+    }
+};
+
